5704dba103e82c7fb1d7540c278d8b471990fcee
강민지 <minji5578@naver.com>
2026-05-11 16:35:04 +0900

    create member_schema.py

---
+++ app/schemas/member_schema.py
@@ -0,0 +1,54 @@
+# schemas/member_schema.py
+
+from pydantic import BaseModel
+from typing import Optional, Any
+from datetime import datetime
+
+# POST /api/members
+class CreateMember(BaseModel):
+    name: str
+    memberId: str
+    memberPw: str
+    birth: datetime.date
+    phone: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "김회원",
+                "memberId": "kimId",
+                "memberPw": "kimPw",
+                "birth": "2003-01-01",
+                "phone": "010-1234-5678",
+            }
+        }
+
+
+# PUT /api/members/{memberId}
+class UpdateMember(BaseModel):
+    name: str
+    memberId: str
+    memberPw: str
+    birth: datetime.date
+    phone: str
+    aiRecommendAlert: bool
+    aiSummary: bool
+    aiSave: bool
+    imageUrl: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "id": 123,
+	            "name": "김회원",
+	            "memberId": "kimId",
+	            "memberPw": "kimPw", 
+	            "birth": "2003-01-01",
+	            "phone": "010-1234-5555",
+	            "imageUrl": "http://",
+	            "aiRecommendAlert": True,
+	            "aiSummary": True,
+	            "aiSave": True,
+	            "createdAt": "2024-02-22T07:47:49.803Z"
+            }
+        }
